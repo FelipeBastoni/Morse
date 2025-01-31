d62608fb5cff2ef63a6b1425826ba5f83f0fa01a
Felipe Bertão Bastoni <163520381+FelipeBastoni@users.noreply.github.com>
2025-01-31 13:12:44 -0300

    Update morse.py

--- morse.py
+++ morse.py
@@ -31,19 +31,49 @@ while (1>0):
     }
 
 
+    txt =   {   ".- ": "A",
+                "-... ": "B",
+                "-.-. ": "C",        
+                "-.. ": "D",
+                ". ": "E", 
+                "..-. ": "F",
+                "--. ": "G",
+                ".... ": "H",
+                ".. ": "I",
+                ".--- ": "J",
+                "-.- ": "K",
+                ".-.. ": "L",
+                "-- ": "M",
+                "-. ": "N",
+                "--- ": "O",
+                ".--. ": "P",
+                "--.- ": "Q",
+                ".-. ": "R",
+                "... ": "S",
+                "- ": "T",
+                "..- ": "U",
+                "...- ": "V",
+                ".-- ": "W",
+                "-..- ": "X",
+                "-.-- ": "Y",
+                "--.. ": "Z",
+                "/ ": " ",
+
+    }
+
 
     LAP = []
     RES = []
+    TEX = []
 
-
-    LAP = input("Digite sua frase para transformar em Morse :D : ")
-    REC = list(LAP)  # Separa em uma lista de caracteres
-
+    LAP = input("Digite sua frase para transformar em Morse :D\n(PS: não funcionará com uso de acentos)\nFrase: ")
+    REC = list(LAP.upper())  # Separa em uma lista de caracteres
+    
 
     rg= len(REC)
 
 
-    for c in range (rg):
+    for c in range(rg):
 
         x= REC[c]
 
